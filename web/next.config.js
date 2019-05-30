84c05e104f1858f529a503d8e549dce8b7f2bfa2
Max Stoiber <mxstbr@github.com>
2019-05-30 16:07:48 +0200

    Nicer typescript formatter

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,19 @@
 const withTypescript = require("@zeit/next-typescript");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const typescriptFormatter = require("react-dev-utils/typescriptFormatter");
 
 module.exports = withTypescript({
   webpack(config, options) {
     // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    if (options.isServer)
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin({
+          async: true,
+          useTypescriptIncrementalApi: true,
+          checkSyntacticErrors: true,
+          formatter: typescriptFormatter
+        })
+      );
 
     return config;
   }
