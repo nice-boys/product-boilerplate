2495a4a571aa7c6f1d3c44464426b1a24ff43724
Max Stoiber <mxstbr@github.com>
2019-05-30 16:16:39 +0200

    Add GraphQL setup with apollo-server-micro and graphql nexus

---
+++ server/routes/index.ts
@@ -0,0 +1,29 @@
+import { queryType, stringArg, makeSchema } from "nexus";
+import { ApolloServer } from "apollo-server-micro";
+
+const Query = queryType({
+  definition(t) {
+    t.string("hello", {
+      args: { name: stringArg({ nullable: true }) },
+      resolve: (_, { name }) => `Hello ${name || "World"}!`
+    });
+  }
+});
+
+const schema = makeSchema({
+  types: [Query],
+  outputs: {
+    schema: __dirname + "/generated/schema.graphql",
+    typegen: __dirname + "/generated/typings.ts"
+  }
+});
+
+const server = new ApolloServer({
+  schema,
+  introspection: true,
+  playground: true
+});
+
+export default server.createHandler({
+  path: "/api"
+});
